7998686c65a0d461e7ecd997f3aa3c182d16d48b
Deathstar606 <dsfardin606@gmail>
2025-04-30 03:28:40 +0600

    minor Edit

--- src/Redux/shared/baseurl.js
+++ src/Redux/shared/baseurl.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'https://vtest-back-tan.vercel.app/';
+export const baseUrl = 'https://vtest-back.vercel.app/';
 //export const baseUrl = 'http://localhost:9000/';
 //export const baseUrl = 'https://test-launch-wheat.vercel.app/';
